cc47e6557054a41235bbf23b41e6b08d504eb29b
AvantikaSaroha <68382062+AvantikaSaroha@users.noreply.github.com>
2021-08-17 18:46:55 +0530

    Create JavaEmail.java

---
+++ src/main/java/online_exam_portal/JavaEmail.java
@@ -0,0 +1,40 @@
+package online_exam_portal;import java.util.Properties;    
+import javax.mail.*;    
+import javax.mail.internet.*;    
+class Mailer{  
+    public static void send(String from,String password,String to,String sub,String msg){  
+          //Get properties object    
+          Properties props = new Properties();    
+          props.put("mail.smtp.host", "smtp.gmail.com");    
+          props.put("mail.smtp.socketFactory.port", "465");    
+          props.put("mail.smtp.socketFactory.class",    
+                    "javax.net.ssl.SSLSocketFactory");    
+          props.put("mail.smtp.auth", "true");    
+          props.put("mail.smtp.port", "465");    
+          //get Session   
+          Session session = Session.getInstance(props,    
+           new javax.mail.Authenticator() {    
+           protected PasswordAuthentication getPasswordAuthentication() {    
+           return new PasswordAuthentication(from,password);  
+           }    
+          });    
+          //compose message    
+          try {    
+           MimeMessage message = new MimeMessage(session);    
+           message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));    
+           message.setSubject(sub);    
+           message.setContent(msg,"text/html");    
+           //send message  
+           Transport.send(message);    
+           System.out.println("message sent successfully");    
+          } catch (MessagingException e) {throw new RuntimeException(e);}    
+             
+    }  
+}  
+public class JavaEmail{    
+ public void send(String email,String subject, String message){    
+     //from,password,to,subject,message  
+     Mailer.send("otp.onlineexamportal@gmail.com","oep.otpmail",email,subject,message);  
+     //change from, password and to  
+ }    
+} 
